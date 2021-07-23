14e0ac16c33ce4ef24d9fdb4b44036da8494fb9a
hardcoregandhi <j.dryan@hotmail.com>
2021-07-23 15:47:14 +0100

    Hack for builders showing as undefined in CreepRoomMap

--- main.js
+++ main.js
@@ -111,25 +111,29 @@ module.exports.loop = function () {
 
     
     global.creepRoomMap = new Map();
-    _.forEach(Game.rooms, r => { _.forEach(Game.creeps, c => {
-        key = r.name+c.memory.role
-        if(c.memory.baseRoomName == r.name) {
-            if (creepRoomMap.get(key)) {
-                creepRoomMap.set(key, creepRoomMap.get(key) + 1)
+    _.forEach(Game.rooms, r => {
+        creepRoomMap.set(r.name+"builder", 0)
+    })
+     
+    _.forEach(Game.rooms, r => { 
+        _.forEach(Game.creeps, c => {
+            key = r.name+c.memory.role
+            if(c.memory.baseRoomName == r.name) {
+                if (creepRoomMap.get(key)) {
+                    creepRoomMap.set(key, creepRoomMap.get(key) + 1)
+                }
+                else {
+                    creepRoomMap.set(key, 1)
+                }
             }
             else {
-                creepRoomMap.set(key, 1)
+                if (creepRoomMap.get(key) == undefined) {
+                    creepRoomMap.set(key, 0)
+                }
             }
-        }
-        else {
-            if (!creepRoomMap.get(key)) {
-                creepRoomMap.set(key, 0)
-            }
-        }
-        
-        creepRoomMap.set(r.name+"csites", r.find(FIND_CONSTRUCTION_SITES).length)
-        
-    } ) } )
+            creepRoomMap.set(r.name+"csites", r.find(FIND_CONSTRUCTION_SITES).length)
+        } )
+    } )
         
     _.forEach(Game.rooms, r => {
             stores = r.find(FIND_STRUCTURES, {
