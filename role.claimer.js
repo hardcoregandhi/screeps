2e5bd466d2d0e4782409888243c608bbc9851b6f
hardcoregandhi <j.dryan@hotmail.com>
2021-07-23 15:42:52 +0100

    Remove destRoomName from claimer

--- role.claimer.js
+++ role.claimer.js
@@ -1,7 +1,7 @@
 global.roleClaimer = {
     name: 'claimer',
-    roleMemory: { memory: { direction: 3 } },
-    BodyParts: [CLAIM, MOVE],
+    roleMemory: { memory: { } },
+    BodyParts: [CLAIM, MOVE, MOVE, MOVE, MOVE, MOVE],
 
     // 1 - UP
     // 3 - RIGHT
@@ -10,29 +10,20 @@ global.roleClaimer = {
 
     /** @param {Creep} creep **/
     run: function (creep) {
-        creep.memory.direction = 3
-        var sources = creep.room.find(FIND_SOURCES);
-        if (creep.memory.direction == -1) {
-            console.log(creep.name + " is awaiting direction")
-        }
-        else {
-            // console.log(creep.memory.)
-            creep.memory.DestSpawnName = Game.map.describeExits(creep.memory.baseRoomName)[creep.memory.direction]
+        creep.say('🏳️');
+        targetRoom = "W19S21"
 
-            if (creep.room.name != creep.memory.DestSpawnName) {
-                const route = Game.map.findRoute(creep.room, creep.memory.DestSpawnName);
-                if (route.length > 0) {
-                    creep.say('Headin oot');
-                    const exit = creep.pos.findClosestByRange(route[0].exit);
-                    creep.moveTo(exit, { visualizePathStyle: { stroke: '#ffffff' } });
-                }
-                else {
-                    creep.say('No route found');
-                }
+        if (creep.room.name != targetRoom) {
+            const route = Game.map.findRoute(creep.room, targetRoom, { maxRooms: 1 });
+            if(route.length > 0) {
+                console.log('Now heading to room '+route[0].room);
+                const exit = creep.pos.findClosestByRange(route[0].exit);
+                creep.moveTo(exit);
             }
-            else if (creep.claimController(creep.room.controller) != OK) {
-                // console.log(creep.claimController(creep.room.controller));
-                creep.moveTo(creep.room.controller, { visualizePathStyle: { stroke: '#ffffff' } });
+        } else {
+            if (creep.claimController(creep.room.controller) != OK) {
+            // console.log(creep.claimController(creep.room.controller));
+             creep.moveTo(creep.room.controller, { visualizePathStyle: { stroke: '#ffffff' } });
             }
         }
     }
