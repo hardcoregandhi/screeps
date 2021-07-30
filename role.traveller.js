6a1bb4a23278d2448f0f658108bf21a511191ccb
hardcoregandhi <j.dryan@hotmail.com>
2021-07-30 19:17:30 +0100

    Fix traveller logic

--- role.traveller.js
+++ role.traveller.js
@@ -13,7 +13,7 @@ global.roleTraveller = {
     run: function (creep) {
         
         creep.say('🏳️');
-        targetRoom = "W17S20"
+        targetRoom = "W20S20"
         
         if (creep.memory.returning && creep.store.getUsedCapacity() == 0) {
             creep.memory.returning = false;
@@ -26,7 +26,6 @@ global.roleTraveller = {
             if (creep.room.name != targetRoom) {
                 const route = Game.map.findRoute(creep.room, targetRoom);
                 if(route.length > 0) {
-                    console.log('Now heading to room '+route[0].room);
                     const exit = creep.pos.findClosestByRange(route[0].exit);
                     moveToTarget(creep, exit, true);
                 }
@@ -36,13 +35,29 @@ global.roleTraveller = {
                         return (structure.structureType == STRUCTURE_POWER_BANK);
                     }
                 });
-                if (creep.attack(sources[0]) != OK) {
-                    ret = creep.moveTo(sources[0], { visualizePathStyle: { stroke: '#ffaa00' } })
+                if(sources.length) {
+                    if (creep.attack(sources[0]) != OK) {
+                        ret = creep.moveTo(sources[0], { visualizePathStyle: { stroke: '#ffaa00' } })
+                    }
+                    return
                 }
+                
+                var power = creep.room.find(FIND_DROPPED_RESOURCES, {
+                    filter: (r) => {
+                        return (r.resourceType == RESOURCE_POWER);
+                    }
+                });
+                if (power.length) {
+                    if (creep.pickup(power[0]) != OK) {
+                        ret = creep.moveTo(power[0], { visualizePathStyle: { stroke: '#ffaa00' } })
+                    }
+                    return
+                }
+                
             }
         } else {
-            if (creep.memory.baseRoomName != targetRoom) {
-                const route = Game.map.findRoute(targetRoom, creep.room);
+            if (creep.memory.baseRoomName != creep.room.name) {
+                const route = Game.map.findRoute(creep.room.name, creep.memory.baseRoomName);
                 if(route.length > 0) {
                     const exit = creep.pos.findClosestByRange(route[0].exit);
                     creep.moveTo(exit);
