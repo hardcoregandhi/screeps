cb0ecfc8fc9079a4bf6e04af276138b4ca2ebb65
hardcoregandhi <j.dryan@hotmail.com>
2021-07-30 19:20:56 +0100

    Let claimer claim and reserve controllers

--- role.claimer.js
+++ role.claimer.js
@@ -11,19 +11,20 @@ global.roleClaimer = {
     /** @param {Creep} creep **/
     run: function (creep) {
         creep.say('🏳️');
-        targetRoom = "W19S21"
+        targetRoom = "W17S21"
 
         if (creep.room.name != targetRoom) {
             const route = Game.map.findRoute(creep.room, targetRoom, { maxRooms: 1 });
             if(route.length > 0) {
-                console.log('Now heading to room '+route[0].room);
                 const exit = creep.pos.findClosestByRange(route[0].exit);
                 creep.moveTo(exit);
             }
         } else {
             if (creep.claimController(creep.room.controller) != OK) {
             // console.log(creep.claimController(creep.room.controller));
-             creep.moveTo(creep.room.controller, { visualizePathStyle: { stroke: '#ffffff' } });
+                if (creep.reserveController(creep.room.controller) != OK) {
+                    creep.moveTo(creep.room.controller, { visualizePathStyle: { stroke: '#ffffff' } });
+                }
             }
         }
     }
