03b72e6dbc49c8072c5aa2b112d7d3cf4084b086
hardcoregandhi <j.dryan@hotmail.com>
2021-07-30 19:11:30 +0100

    Remove all unused tower behaviours

--- tower.js
+++ tower.js
@@ -5,47 +5,22 @@ var roleTower = {
 
         percentageBased = true;
 
-        var damagedStructures = tower.room.find(FIND_STRUCTURES, {
-            filter: (structure) => (
-                (
-                    (
-                        Math.round((structure.hits / structure.hitsMax) * 100 < 50)
-                    )
-                )
-            )
-        });
-        
-        var hitsBasedStructures = tower.room.find(FIND_STRUCTURES, {
-            filter: (structure) => (
-                (
-                    (
-                        structure.hits < 10000
-                    )
-                )
-            )
-        });
-
         var highlyDamagedStructFound = tower.room.find(FIND_STRUCTURES, {
             filter: (structure) => (
                 (
                     (
+                        structure.structureType == STRUCTURE_ROAD &&
                         Math.round((structure.hits / structure.hitsMax) * 100 < 1)
+                    )
+                    ||
+                    (
+                        structure.structureType == STRUCTURE_WALL &&
+                        Math.round((structure.hits / structure.hitsMax) * 100 < 0.01)
                     )
                 )
             )
         });
         
-        var roadsFiftyPercentStructs = tower.room.find(FIND_STRUCTURES, {
-            filter: (structure) => (
-                (
-                    (
-                        structure.structureType == STRUCTURE_ROAD &&
-                        Math.round((structure.hits / structure.hitsMax) * 100 < 50)
-                    )
-                )
-            )
-        });
-
         var customStructureSpecificPercentLimits = tower.room.find(FIND_STRUCTURES, {
             filter: (structure) => (
                 (
@@ -57,7 +32,7 @@ var roleTower = {
                         ||
                         (
                             structure.structureType == STRUCTURE_RAMPART &&
-                            Math.round((structure.hits / structure.hitsMax) * 100 < 0.5)
+                            Math.round((structure.hits / structure.hitsMax) * 100 < 0.6)
                         )
                         ||
                         (
@@ -70,6 +45,19 @@ var roleTower = {
         });
         
         var closestHostile = tower.pos.findClosestByRange(FIND_HOSTILE_CREEPS);
+        var allHostiles = tower.room.find(FIND_HOSTILE_CREEPS);
+        var healer = null
+        for (var h of allHostiles) {
+            console.log(h)
+            if (h.body.includes(HEAL)){
+                healer = h
+            }
+        }
+        
+        if (healer) {
+            tower.attack(healer);
+        }
+        
         if (closestHostile) {
             tower.attack(closestHostile);
             return
@@ -85,63 +73,11 @@ var roleTower = {
             return
         }
         
-        if (1){
-            rangeBased = false
-            percentageBased = false
-            hitsBased = false
-            roadsFiftyPercent = false
+        if(highlyDamagedStructFound.length) {
+            highlyDamagedStructFound.sort((a, b) => a.hits - b.hits);
+            mostDamagedStructure = highlyDamagedStructFound[0]
 
-            customStructureSpecificPercent = true
-        }
-        else if (highlyDamagedStructFound) {
-            rangeBased = false
-            percentageBased = false
-            hitsBased = true
-        }
-        else if (damagedStructures.length > 10) {
-            rangeBased = true
-            percentageBased = false
-            hitsBased = false
-
-
-        } else {
-            rangeBased = false
-            percentageBased = true
-            hitsBased = false
-        }
-
-        if (percentageBased) {
-            damagedStructures.sort((a, b) => Math.round((a.hits / a.hitsMax) * 100) - Math.round((b.hits / b.hitsMax) * 100));
-            mostDamagedStructure = damagedStructures[0]
-
-            for (var t of damagedStructures) {
-                new RoomVisual().text(Math.round((t.hits / t.hitsMax) * 100) + "%", t.pos, { align: 'right', font: 0.2 });
-            }
-            // damagedStructures.forEach((e, i) => (new RoomVisual().text(Math.round((e.hits / e.hitsMax) * 100) + " Pri: " + i, e.pos, {align: 'left', font: 0.2})));
-            // var arr = []; damagedStructures.forEach((i) => arr.push(i.hits)); console.log(arr)
-
-            if (mostDamagedStructure) {
-                tower.room.visual.circle(mostDamagedStructure.pos, { stroke: 'green', radius: 0.5, lineStyle: 'dashed', fill: 'transparent' });
-                tower.repair(mostDamagedStructure);
-            }
-        } else if (rangeBased) {
-            damagedStructures.sort((a, b) => a.hits - b.hits);
-            closestTarget = tower.pos.findClosestByRange(damagedStructures)
-
-            for (var t of damagedStructures) {
-                new RoomVisual().text(t.hits, t.pos, { align: 'right', font: 0.2 });
-            }
-            // damagedStructures.forEach((e, i) => (new RoomVisual().text(e.hits + " Order: " + i, e.pos, {align: 'left'}))); 
-
-            if (closestTarget) {
-                tower.room.visual.circle(closestTarget.pos, { stroke: 'green', radius: 0.5, lineStyle: 'dashed', fill: 'transparent' });
-                tower.repair(closestTarget);
-            }
-        } else if(hitsBased) {
-            hitsBasedStructures.sort((a, b) => a.hits - b.hits);
-            mostDamagedStructure = hitsBasedStructures[0]
-
-            for (var t of hitsBasedStructures) {
+            for (var t of highlyDamagedStructFound) {
                 new RoomVisual().text(t.hits, t.pos, { align: 'right', font: 0.2 });
             }
             // damagedStructures.forEach((e, i) => (new RoomVisual().text(e.hits + " Order: " + i, e.pos, {align: 'left'}))); 
@@ -150,22 +86,9 @@ var roleTower = {
                 tower.room.visual.circle(mostDamagedStructure.pos, { stroke: 'green', radius: 0.5, lineStyle: 'dashed', fill: 'transparent' });
                 tower.repair(mostDamagedStructure);
             }
-        } else if (roadsFiftyPercent) {
-            roadsFiftyPercentStructs.sort((a, b) => a.hits - b.hits);
-            closestTarget = tower.pos.findClosestByRange(roadsFiftyPercentStructs)
-
-
-            for (var t of roadsFiftyPercentStructs) {
-                new RoomVisual().text(t.hits, t.pos, { align: 'right', font: 0.2 });
-            }
-            // damagedStructures.forEach((e, i) => (new RoomVisual().text(e.hits + " Order: " + i, e.pos, {align: 'left'}))); 
-
-            if (closestTarget) {
-                tower.room.visual.circle(closestTarget.pos, { stroke: 'green', radius: 0.5, lineStyle: 'dashed', fill: 'transparent' });
-                tower.repair(closestTarget);
-                return
-            }
-        } else if (customStructureSpecificPercent) {
+        }
+        
+        else { //customStructureSpecificPercent
             customStructureSpecificPercentLimits.sort((a, b) => a.hits - b.hits);
             closestTarget = tower.pos.findClosestByRange(customStructureSpecificPercentLimits)
 
