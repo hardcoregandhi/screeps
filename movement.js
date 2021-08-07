f42fa386634373e3045b3916dacf15773add033c
hardcoregandhi <j.dryan@hotmail.com>
2021-08-07 02:47:54 +0100

    Attempt to improve movement functions

--- movement.js
+++ movement.js
@@ -1,13 +1,14 @@
-global.moveToTarget = function (creep, target, canUseSwamp) {
-    canUseSwamp = true;
+global.moveToTarget = function (creep, target, canUseSwamp = true) {
+    canUseSwamp = true
     if (canUseSwamp) {
         creep.moveTo(target, {
             visualizePathStyle: { stroke: "#ffffff" },
-            maxRooms: 1,
+            maxRooms: 0,
         });
     } else {
         const path = creep.room.findPath(creep.pos, target, {
             ignoreCreeps: false,
+            maxRooms: 0,
         });
         if (path.length) {
             roomPos = new RoomPosition(path[0].x, path[0].y, creep.room.name);
@@ -31,14 +32,13 @@ global.moveToTarget = function (creep, target, canUseSwamp) {
 };
 
 global.moveToRoom = function (creep, targetRoom) {
-    let from = creep.pos;
+    let from = creep.room.name;
     let to = targetRoom;
 
     // Use `findRoute` to calculate a high-level plan for this path,
     // prioritizing highways and owned rooms
-    let allowedRooms = { [from.roomName]: true };
-    Game.map
-        .findRoute(from.roomName, to.roomName, {
+    let allowedRooms = { [from]: true };
+    Game.map.findRoute(from, to, {
             routeCallback(roomName) {
                 let parsed = /^[WE]([0-9]+)[NS]([0-9]+)$/.exec(roomName);
                 let isHighway = parsed[1] % 10 === 0 || parsed[2] % 10 === 0;
@@ -64,4 +64,6 @@ global.moveToRoom = function (creep, targetRoom) {
     });
 
     console.log(ret.path);
+    let pos = ret.path[0];
+    creep.move(creep.pos.getDirectionTo(pos));
 };
