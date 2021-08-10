c1d6de007d6aef6740aa2ce172002b6cdf0c1565
hardcoregandhi <j.dryan@hotmail.com>
2021-08-10 18:51:07 +0100

    Add a new multiRoom move and a pathfinder move from the docs

--- movement.js
+++ movement.js
@@ -4,11 +4,46 @@ global.moveToTarget = function (creep, target, canUseSwamp = true) {
         creep.moveTo(target, {
             visualizePathStyle: { stroke: "#ffffff" },
             maxRooms: 0,
+            maxOps: 100000,
         });
     } else {
         const path = creep.room.findPath(creep.pos, target, {
             ignoreCreeps: false,
             maxRooms: 0,
+        });
+        if (path.length) {
+            roomPos = new RoomPosition(path[0].x, path[0].y, creep.room.name);
+            isSwamp = new Room.Terrain(creep.room.name).get(path[0].x, path[0].y) == TERRAIN_MASK_SWAMP;
+            isPath = roomPos.lookFor(LOOK_STRUCTURES).length != 0;
+            for (var pathStep of path) {
+                if (!isSwamp || (isSwamp && isPath)) {
+                    creep.room.visual.circle(pathStep, { fill: "red" });
+                }
+            }
+            if (!isSwamp || (isSwamp && isPath)) {
+                return creep.moveTo(path[0].x, path[0].y, {
+                    visualizePathStyle: { stroke: "#ffffff" },
+                    maxRooms: 1,
+                });
+            }
+        } else {
+            return -1;
+        }
+    }
+};
+
+global.moveToMultiRoomTarget = function (creep, target, canUseSwamp = true) {
+    canUseSwamp = true;
+    if (canUseSwamp) {
+        creep.moveTo(target, {
+            visualizePathStyle: { stroke: "#ffffff" },
+            maxOps: 100000,
+            maxRooms: 16,
+        });
+    } else {
+        const path = creep.room.findPath(creep.pos, target, {
+            ignoreCreeps: false,
+            maxRooms: 16,
         });
         if (path.length) {
             roomPos = new RoomPosition(path[0].x, path[0].y, creep.room.name);
@@ -68,3 +103,53 @@ global.moveToRoom = function (creep, targetRoom) {
     let pos = ret.path[0];
     creep.move(creep.pos.getDirectionTo(pos));
 };
+
+global.usePathfinder = function (creep, goal) {
+  let goals = _.map(creep.room.find(FIND_SOURCES), function(source) {
+    // We can't actually walk on sources-- set `range` to 1 
+    // so we path next to it.
+    return { pos: source.pos, range: 1 };
+  });
+
+  let ret = PathFinder.search(
+    creep.pos, goals,
+    {
+      // We need to set the defaults costs higher so that we
+      // can set the road cost lower in `roomCallback`
+      plainCost: 2,
+      swampCost: 10,
+
+      roomCallback: function(roomName) {
+
+        let room = Game.rooms[roomName];
+        // In this example `room` will always exist, but since 
+        // PathFinder supports searches which span multiple rooms 
+        // you should be careful!
+        if (!room) return;
+        let costs = new PathFinder.CostMatrix;
+
+        room.find(FIND_STRUCTURES).forEach(function(struct) {
+          if (struct.structureType === STRUCTURE_ROAD) {
+            // Favor roads over plain tiles
+            costs.set(struct.pos.x, struct.pos.y, 1);
+          } else if (struct.structureType !== STRUCTURE_CONTAINER &&
+                     (struct.structureType !== STRUCTURE_RAMPART ||
+                      !struct.my)) {
+            // Can't walk through non-walkable buildings
+            costs.set(struct.pos.x, struct.pos.y, 0xff);
+          }
+        });
+
+        // Avoid creeps in the room
+        room.find(FIND_CREEPS).forEach(function(creep) {
+          costs.set(creep.pos.x, creep.pos.y, 0xff);
+        });
+
+        return costs;
+      },
+    }
+  );
+
+  let pos = ret.path[0];
+  creep.move(creep.pos.getDirectionTo(pos));
+}
