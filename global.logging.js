32b001a6c5ae5c165ed0a62154900d6cacda201f
hardcoregandhi <j.dryan@hotmail.com>
2021-09-05 22:15:28 +0100

    Add a logging level colouring sytem

---
+++ global.logging.js
@@ -0,0 +1,18 @@
+global.LOGGING_ENABLED = true;
+global.logging = function (bool) {
+    global.LOGGING_ENABLED = bool;
+};
+global.log = {
+    log: function log(arg) {
+        if (global.LOGGING_ENABLED) return console.log(arg);
+    },
+    warn: function warn(arg) {
+        if (global.LOGGING_ENABLED) return console.log("<span style=color:#FFBF3F>" + arg + "</span>");
+    },
+    err: function err(arg) {
+        if (global.LOGGING_ENABLED) return console.log("<span style=color:#D18F98>" + arg + "</span>");
+    },
+    error: function error(arg) {
+        if (global.LOGGING_ENABLED) return console.log("<span style=color:#D18F98>" + arg + "</span>");
+    },
+};
