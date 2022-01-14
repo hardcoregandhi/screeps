d9bf5c1665972cd799753fc1e307b43afa1b0670
hardcoregandhi <j.dryan@hotmail.com>
2022-01-14 18:56:34 +0000

    Update powMover to use the new room Memory structs

--- role.powMover.js
+++ role.powMover.js
@@ -12,6 +12,7 @@ const job = {
     RETURNING: 2,
 };
 
+global.rolePowMover.name = "powMover";
 global.rolePowMover.run = function (creep) {
     // Lost creeps return home
     if (creep.room.name != creep.memory.baseRoomName) {
@@ -19,44 +20,49 @@ global.rolePowMover.run = function (creep) {
         if (route.length > 0) {
             creep.say("Headin oot");
             const exit = creep.pos.findClosestByRange(route[0].exit);
-            moveToTarget(creep, exit, true);
+            moveToTarget(creep, exit);
         } else {
             creep.say("No route found");
         }
         return;
     }
-    var power_spawns = creep.room.find(FIND_STRUCTURES).filter((structure) => structure.structureType == STRUCTURE_POWER_SPAWN);
-    var power_spawns_pow = power_spawns.filter((structure) => structure.structureType == STRUCTURE_POWER_SPAWN && structure.store.getUsedCapacity(RESOURCE_POWER) < 20);
-    var power_spawns_en = power_spawns.filter((structure) => structure.structureType == STRUCTURE_POWER_SPAWN && structure.store.getFreeCapacity(RESOURCE_ENERGY) > 0);
+    var power_spawn = Game.getObjectById(Memory.rooms[creep.memory.baseRoomName].structs.pspawn.id)
+    var mainStorage = Game.getObjectById(Memory.rooms[creep.memory.baseRoomName].mainStorage)
+    var power_spawn_pow = power_spawn.store.getUsedCapacity(RESOURCE_POWER) < 20;
+    var power_spawn_en = power_spawn.store.getFreeCapacity(RESOURCE_ENERGY) > 0;
+    
+    power_spawn.processPower();
+    
+    if (mainStorage.store.getUsedCapacity(RESOURCE_POWER) == 0 && power_spawn.store.getUsedCapacity(RESOURCE_POWER) == 0) {
+        creep.memory.DIE = true;
+        return;
+    }
 
-    if (power_spawns_pow.length) {
+    if (power_spawn_pow) {
         creep.memory.job = job.MOVING_POWER;
-    } else if (power_spawns_en.length) {
+    } else if (power_spawn_en) {
         creep.memory.job = job.MOVING_ENERGY;
     } else {
         creep.memory.job = job.RETURNING;
     }
 
     // If creep is holding non-energy, deposit it first
-    var storage = creep.pos.findClosestByPath(FIND_STRUCTURES).filter((structure) => {
-        return structure.structureType == STRUCTURE_STORAGE && structure.store.getFreeCapacity(RESOURCE_ENERGY) > 0;
-    });
     for (const resourceType in creep.store) {
         switch (creep.memory.job) {
             case job.MOVING_POWER:
                 if (resourceType != RESOURCE_POWER) {
-                    if (creep.transfer(storage, resourceType) != OK) {
+                    if (creep.transfer(mainStorage, resourceType) != OK) {
                         // console.log(creep.transfer(storage, resourceType) )
-                        moveToTarget(creep, storage, true);
+                        moveToTarget(creep, mainStorage);
                         return;
                     }
                 }
                 break;
             case job.MOVING_ENERGY:
                 if (resourceType != RESOURCE_ENERGY) {
-                    if (creep.transfer(storage, resourceType) != OK) {
+                    if (creep.transfer(mainStorage, resourceType) != OK) {
                         // console.log(creep.transfer(storage, resourceType) )
-                        moveToTarget(creep, storage, true);
+                        moveToTarget(creep, mainStorage);
                         return;
                     }
                 }
@@ -73,51 +79,42 @@ global.rolePowMover.run = function (creep) {
         creep.say("dropping");
     }
 
-    if (creep.ticksToLive < 200 && creep.memory.moving) {
+    if ((creep.ticksToLive < 300 || creep.memory.healing) && (creep.memory.noHeal == undefined || creep.memory.noHeal != true)) {
         creep.say("healing");
         creep.memory.healing = true;
-        var targets = creep.room.find(FIND_STRUCTURES).filter((structure) => {
-            return structure.structureType == STRUCTURE_SPAWN && structure.store.getFreeCapacity(RESOURCE_ENERGY) > 0;
-        });
-        if (creep.transfer(targets[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
-            moveToTarget(creep, targets[0], false);
-        }
-        return;
+        if (returnToHeal(creep, creep.memory.baseRoomName)) return;
     }
 
     if (!creep.memory.moving) {
         // creep.say("hello")
-        var targets = creep.room.find(FIND_STRUCTURES).filter((structure) => {
-            return structure.structureType == STRUCTURE_STORAGE && structure.store.getUsedCapacity() > 0;
-        });
-        if (targets.length) {
-            creep.say("m2storage");
-            switch (creep.memory.job) {
-                case job.MOVING_POWER:
-                    if (creep.withdraw(targets[0], RESOURCE_POWER) != OK) {
-                        moveToTarget(creep, targets[0], true);
+        
+        creep.say("m2storage");
+        switch (creep.memory.job) {
+            case job.MOVING_POWER:
+                if (creep.withdraw(mainStorage, RESOURCE_POWER) != OK) {
+                    moveToTarget(creep, mainStorage);
+                    if (mainStorage.store.getUsedCapacity(RESOURCE_POWER) == 0) {
+                        creep.memory.moving = true
                     }
-                    break;
-                case job.MOVING_ENERGY:
-                    if (creep.withdraw(targets[0], RESOURCE_ENERGY) != OK) {
-                        moveToTarget(creep, targets[0], true);
-                    }
-                    break;
-            }
-        } else creep.say("no power");
+                }
+                break;
+            case job.MOVING_ENERGY:
+                if (creep.withdraw(mainStorage, RESOURCE_ENERGY) != OK) {
+                    moveToTarget(creep, mainStorage);
+                }
+                break;
+        }
     } else {
-        if (power_spawns) {
-            target = creep.pos.findClosestByPath(power_spawns);
+        if (power_spawn) {
             switch (creep.memory.job) {
                 case job.MOVING_POWER:
-                    if (creep.transfer(target, RESOURCE_POWER) != OK) {
-                        moveToTarget(creep, target, false);
+                    if (creep.transfer(power_spawn, RESOURCE_POWER) != OK) {
+                        moveToTarget(creep, power_spawn);
                     }
                     break;
                 case job.MOVING_ENERGY:
-                    target = creep.pos.findClosestByPath(power_spawns);
-                    if (creep.transfer(target, RESOURCE_ENERGY) != OK) {
-                        moveToTarget(creep, target, false);
+                    if (creep.transfer(power_spawn, RESOURCE_ENERGY) != OK) {
+                        moveToTarget(creep, power_spawn);
                     }
                     break;
             }
