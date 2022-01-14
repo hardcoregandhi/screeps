061ad546cbfbd8429ec562b37edfee534ce8321a
hardcoregandhi <j.dryan@hotmail.com>
2022-01-14 18:44:21 +0000

    Make getRandomInt global It's used elsewhere now, should probably be made common

--- manage.createCreeps.js
+++ manage.createCreeps.js
@@ -1,4 +1,4 @@
-function getRandomInt(min = 100, max = 999) {
+getRandomInt = function(min = 100, max = 999) {
     min = Math.ceil(min);
     max = Math.floor(max);
     return Math.floor(Math.random() * (max - min + 1)) + min;
