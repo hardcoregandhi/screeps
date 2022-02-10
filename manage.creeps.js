5da16ac60e50a610e656955220eec08a5d79d6c5
hardcoregandhi <j.dryan@hotmail.com>
2022-02-10 21:29:36 +0000

    Switch the creep role if list to use eval role should make it easier to add new roles

--- manage.creeps.js
+++ manage.creeps.js
@@ -74,133 +74,22 @@ global.runCreeps = function () {
                 }
             }
 
-            // if(creep.name == "Soldier_748") {
-            //     if(creep.memory.healing != true && Game.rooms[creep.memory.targetRoomName] == undefined) {
-            //         // moveToRoom(creep, creep.memory.targetRoomName)
-            //         roomPath = Game.map.findRoute(creep.room.name, creep.memory.targetRoomName)
-            //         const exit = creep.pos.findClosestByRange(roomPath[0].exit);
-            //         if(creep.fatigue == 0) {
-            //             creep.moveTo(exit);
-
-            //             if(creep.memory._move != undefined) {
-            //                 targetPos = new RoomPosition(creep.memory._move.dest.x, creep.memory._move.dest.y, creep.memory._move.dest.room)
-            //                 if(targetPos.lookFor(LOOK_STRUCTURES).length != 0) {
-            //                     log.log("found structure in paath")
-            //                     usePathfinder(creep, { pos: new RoomPosition(25, 25, roomPath[0].room), range: 1 })
-            //                 }
-            //             }
-            //         }
-            //         continue
-            //     }
-            // }
-
-            if (creep.name == "Claimer_755") {
-                if (creep.ticksToLive < 300 || creep.memory.healing) {
-                    creep.say("healing");
-                    creep.memory.healing = true;
-                    if (returnToHeal(creep, creep.memory.baseRoomName)) return;
-                }
-            }
-
-            if (creep.memory.role == "traveller") {
-                roleTraveller.run(creep);
-                continue;
-            }
-            // var closestHostile = creep.pos.findClosestByRange(FIND_HOSTILE_CREEPS);
-            // if (closestHostile) {
-            //     roleDefence.run(creep)
-            //     continue
-            // }
-            // if (creep.memory.role == "upgrader" && creepRoomMap.get(creep.room.name + "harvester") < 1) {
-            //     roleHarvester.run(creep);
-            //     continue;
-            // }
-            if (creep.memory.role == "harvester") {
-                roleHarvester.run(creep, focusHealing);
-            }
-            if (creep.memory.role == "upgrader") {
-                roleUpgrader.run(creep);
-            }
-            if (creep.memory.role == "builder") {
-                roleBuilder.run(creep);
-            }
-            if (creep.memory.role == "builderExt") {
-                roleBuilderExt.run(creep);
-            }
-            if (creep.memory.role == "claimer") {
-                roleClaimer.run(creep);
-            }
-            if (creep.memory.role == "harvesterExt") {
-                roleHarvesterExt.run(creep);
-            }
-            if (creep.memory.role == "harvesterDeposit") {
-                roleHarvesterDeposit.run(creep);
-            }
-            if (creep.memory.role == "harvSup") {
-                roleHarvSup.run(creep);
-            }
-            if (creep.memory.role == "moverExt" || creep.memory.role == "moverExtRepair") {
-                roleMoverExt.run(creep);
-            }
-            if (creep.memory.role == "moverLink") {
-                roleMoverLink.run(creep);
-            }
-            if (creep.memory.role == "trucker") {
-                roleTrucker.run(creep);
-            }
-            if (creep.memory.role == "soldier") {
-                roleSoldier.run(creep);
-            }
-            if (creep.memory.role == "gunner") {
-                roleGunner.run(creep);
-            }
-            if (creep.memory.role == "sieger") {
-                roleSieger.run(creep);
-            }
-            if (creep.memory.role == "powHarvester") {
-                rolePowHarvester.run(creep);
-            }
-            if (creep.memory.role == "powMover") {
-                rolePowMover.run(creep);
-            }
-            if (creep.memory.role == "doctor") {
-                roleDoctor.run(creep);
-            }
-            if (creep.memory.role == "invader") {
-                roleInvader.run(creep);
-            }
-            if (creep.memory.role == "explorer") {
-                roleExplorer.run(creep);
-            }
-            if (creep.memory.role == "cleaner") {
-                roleCleaner.run(creep);
-            }
-            if (creep.memory.role == "healer") {
-                roleHealer.run(creep);
-            }
-            if (creep.memory.role == "wanderer") {
-                roleWanderer.run(creep);
-            }
-            if (creep.memory.role == "healerChase") {
-                roleHealerChase.run(creep);
-            }
-            if (creep.memory.role == "mover") {
-                if (creep.room.energyAvailable > 600 && Memory.rooms[creep.room.name].scav == true) {
-                    roleScavenger.run(creep);
-                    // console.log(creep.name, "scav")
-                } else {
-                    roleMover.run(creep);
-                    // console.log(creep.name, "mover")
-                }
-            }
-            if (creep.memory.role == "engineer") {
-                eval("role" + _.capitalize(creep.memory.role)).run(creep);
-            }
-            if (creep.memory.role == "raider") {
-                eval("role" + _.capitalize(creep.memory.role)).run(creep);
-            }
-            if (creep.memory.role == "harvesterMineral") {
-                eval("role" + _.capitalize(creep.memory.role)).run(creep);
+            switch (creep.memory.role) {
+                case "mover":
+                    if (creep.room.energyAvailable > 600 && Memory.rooms[creep.room.name].scav == true) {
+                        roleScavenger.run(creep);
+                        // console.log(creep.name, "scav")
+                    } else {
+                        roleMover.run(creep);
+                        // console.log(creep.name, "mover")
+                    }
+                    break
+                case "moverExt":
+                case "moverExtRepair":
+                    roleMoverExt.run(creep);
+                    continue;
+                default:
+                    eval("role" + _.capitalize(creep.memory.role)).run(creep);
             }
         } catch (e) {
             console.log(`${e}`);
