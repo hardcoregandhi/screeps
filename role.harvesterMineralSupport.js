326ab269012b2184cf47a800e8cc365173e0716f
hardcoregandhi <j.dryan@hotmail.com>
2023-06-10 17:48:22 +0100

    Add body part loop to HarvesterMineralSupport

--- role.harvesterMineralSupport.js
+++ role.harvesterMineralSupport.js
@@ -4,3 +4,6 @@ roleHarvesterMineralSupport.BodyParts = [
     CARRY, CARRY, CARRY, CARRY, CARRY, CARRY, CARRY, CARRY, CARRY, CARRY, 
     MOVE, MOVE, MOVE, MOVE, MOVE,
     ];
+roleHarvesterMineralSupport.baseBodyParts = [MOVE,CARRY,CARRY]
+roleHarvesterMineralSupport.bodyLoop = [MOVE,CARRY,CARRY]
+roleHarvesterMineralSupport.bodyPartsMaxCount = 13
