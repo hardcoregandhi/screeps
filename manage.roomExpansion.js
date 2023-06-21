970b4aef563a92b3d2a7707472f82875110e9a1e
hardcoregandhi <j.dryan@hotmail.com>
2023-06-21 18:26:38 +0100

    Only spawn roleTruckers if there is >50000 energy

--- manage.roomExpansion.js
+++ manage.roomExpansion.js
@@ -55,8 +55,9 @@ roomExpansion = function (myRoom) {
                         (Game.rooms[targetRoomName] != undefined &&
                             Game.rooms[targetRoomName].controller.my == true &&
                             Memory.rooms[targetRoomName].mainSpawn == undefined &&
-                            creepRoomMap.get(`${myRoom}truckerTarget${targetRoomName}`) == undefined) ||
-                        creepRoomMap.get(`${myRoom}truckerTarget${targetRoomName}`) < 10
+                            target.store.getUsedCapacity(RESOURCE_ENERGY) >= 50000 &&
+                            (creepRoomMap.get(`${myRoom}truckerTarget${targetRoomName}`) == undefined) ||
+                        creepRoomMap.get(`${myRoom}truckerTarget${targetRoomName}`) < 10)
                     ) {
                         console.log(`roomExpansion: ${myRoom} targetting ${targetRoomName} spawning roleTrucker`);
                         spawnCreep(roleTrucker, "auto", { memory: { targetRoomName: targetRoomName, dumper: true } }, myRoom);
